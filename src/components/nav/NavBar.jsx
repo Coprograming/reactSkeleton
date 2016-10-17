7dc8ecc936321de121137433a79c993ee681808d
the_kaseys <coprograming@gmail.com>
2016-10-17 14:23:28 -0600

    moved file

--- src/components/nav/NavBar.jsx
+++ src/components/nav/NavBar.jsx
@@ -4,14 +4,14 @@ var NavBar = React.createClass({
     render: function() {
         return (
             <div>
-                <nav className="navbar-default">
+                <nav className="navbar navbar-default">
                     <div className="navbar-header">
                         <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#nav-collapse">
                             <span className="icon-bar"></span>
                             <span className="icon-bar"></span>
                             <span className="icon-bar"></span>
                         </button>
-                        <a className="navbar-brand" herf="#">Product Shop</a>
+                        <a className="navbar-brand" href="#">Product Shop</a>
                     </div>
                 </nav>
             </div>
